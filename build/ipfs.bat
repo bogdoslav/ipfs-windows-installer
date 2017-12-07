b90cebaa6343fd1e8d9532cfcd3963786c3d98ba
Viacheslav V Bogdanov <viacheslav.v.bogdanov@gmail.com>
2017-12-07 19:58:46 +0700

    Cleanup

--- build/ipfs.bat
+++ build/ipfs.bat
@@ -1,4 +1,4 @@
 ipfs.exe init
 start /B "" "ipfs.exe" daemon>%temp%\IPFS_output.txt 
-sleep 1
+ping 127.0.0.1 -n 6 > nul
 start "" http://localhost:8080/ipfs/QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG/readme
